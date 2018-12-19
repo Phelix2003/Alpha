63b1f5bfce4415db3ef20fc28b8cc4c315f02bb0
JHA <jonathan.halleux@gmail.com>
2018-12-19 21:31:09 +0100

    Edit / create Item done.

--- Alpha/Controllers/MenuController.cs
+++ Alpha/Controllers/MenuController.cs
@@ -5,11 +5,17 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
 using Alpha.Models;
 using System.Globalization;
+using System.Net;
+using System.Data.Entity;
+
 
 namespace Alpha.Controllers
 {
@@ -35,62 +41,107 @@ namespace Alpha.Controllers
         }
 
         [HttpGet]
-        public ActionResult CreateItem()
+        public ActionResult CreateItem(CreateItemViewModel itemViewModel)
         {
-
-            return View();
+            if(itemViewModel.ItemId == 0)
+            {
+                itemViewModel.IsAvailable = true;
+            }
+            return View(itemViewModel);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> CreateItem( CreateItemViewModel itemViewModel  )
+        [ActionName("CreateItem")]
+        public async Task<ActionResult> CreateItemPost(CreateItemViewModel itemViewModel)
         {
             if (ModelState.IsValid)
             {
-                Item item = new Item
+                if (itemViewModel.Image != null)
                 {
-                    Name = itemViewModel.Name,
-                    // To do : add additional control on the convert feature (catch exceptions) 
-                    UnitPrice = Convert.ToDecimal(itemViewModel.UnitPrice, new CultureInfo("en-US"))
-                };
-                DbManager.Items.Add(item);
-                await DbManager.SaveChangesAsync();
-                
-                return RedirectToAction("CreateItem");
+                    Item item = new Item()
+                    {
+                        IsAvailable = itemViewModel.IsAvailable,
+                        Name = itemViewModel.Name,
+                        UnitPrice = Convert.ToDecimal(itemViewModel.UnitPrice, new CultureInfo("en-US")),
+                        Image = ProcessFileToImage(itemViewModel.Image)
+                    };
+
+                    if (item.Image == null)
+                    {
+                        ViewBag.Message = "Le format de l'image n'est pas correct";
+                        return View(itemViewModel);
+                    }
+                    else
+                    {
+                        ViewBag.Message = "File uploaded successfully";
+                    }
+
+                    DbManager.Items.Add(item);
+                    await DbManager.SaveChangesAsync();
+                    ViewBag.Message = "File uploaded successfully";
+                }
+                else
+                {
+                    ViewBag.Message = "Introduisez une image";
+                }
             }
-            else
+            return View(itemViewModel);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> EditItem(int? id)
+        {
+            if (id == null)
             {
-                return View();
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Item item = await DbManager.Items.FirstAsync(r => r.ItemId == id);
+            if (item == null)
+            {
+                return HttpNotFound();
             }
 
+            return View(new EditItemViewModel()
+            {
+                ItemId = item.ItemId,
+                Name = item.Name,
+                IsAvailable = item.IsAvailable,
+                UnitPrice = item.UnitPrice.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture)
+            });
         }
 
         [HttpPost]
-        public async Task<ActionResult> ItemUploadImage(HttpPostedFileBase file)
+        [ValidateAntiForgeryToken]
+        [ActionName("EditItem")]
+        public async Task<ActionResult> EditItemPost(CreateItemViewModel itemViewModel)
         {
-            if (file != null && file.ContentLength > 0)
+            if (ModelState.IsValid)
             {
-                // for more details see here https://stackoverflow.com/questions/1171696/how-do-you-convert-a-httppostedfilebase-to-an-image
-                // To do ajouter la gestion d'exceptions; 
-                Image image = Image.FromStream(file.InputStream, true, true);
-                Item item = new Item
+                Item item = await DbManager.Items.FirstAsync(r => r.ItemId == itemViewModel.ItemId);
+
+                item.IsAvailable = itemViewModel.IsAvailable;
+                item.Name = itemViewModel.Name;
+                item.UnitPrice = Convert.ToDecimal(itemViewModel.UnitPrice, new CultureInfo("en-US"));
+
+                if (itemViewModel.Image != null)
                 {
-                    Name = "Picture test",
-                    Image = imageToByteArray(image)
-                };
-                DbManager.Items.Add(item);
+                    item.Image = ProcessFileToImage(itemViewModel.Image);
+                    if (item.Image == null)
+                    {
+                        ViewBag.Message = "Error while uploading the file";
+                    }
+                    else
+                    {
+                        ViewBag.Message = "File uploaded successfully";
+                    }
+                }
                 await DbManager.SaveChangesAsync();
-
-
-
-                ViewBag.Message = "File uploaded successfully";
             }
-            else
-            {
-                ViewBag.Message = "You have not specified a file.";
-            }
-            return RedirectToAction("CreateItem");
+            return View("EditItem", new { id = itemViewModel.ItemId });
         }
+
+
 
         private byte[] imageToByteArray(Image imageIn)
         {
@@ -106,5 +157,63 @@ namespace Alpha.Controllers
             return returnImage;
         }
 
+        public async Task<ActionResult> RenderItemPhoto(int ItemId)
+        {
+            Item item = await DbManager.Items.FindAsync(ItemId);
+            byte[] photo = item.Image;
+            return File(photo, "image/jpeg");
+        }
+
+        private byte[] ProcessFileToImage(HttpPostedFileBase dataIn)
+        {
+            try
+            {
+                const double FinalImageSize = 150; // in pixels
+
+                int newWidth;
+                int newHeight;
+
+                using (Image image = Image.FromStream(dataIn.InputStream, true, true))
+                {
+                    // Rescaling image size to be done here,  before saving it in the DB. See https://stackoverflow.com/questions/1171696/how-do-you-convert-a-httppostedfilebase-to-an-image
+                    //https://stackoverflow.com/questions/1922040/how-to-resize-an-image-c-sharp
+
+                    //var destRect = new Rectangle(0, 0, newWidth, newHeight);
+                    if (image.Height < image.Width) // Image scaling while conserving the ratio. 
+                    {
+                        newWidth = (int)FinalImageSize;
+                        newHeight = (int)((double)image.Height * (double)FinalImageSize / (double)image.Width);
+                    }
+                    else
+                    {
+                        newHeight = (int)FinalImageSize;
+                        newWidth = (int)((double)image.Width * (double)FinalImageSize / (double)image.Height);
+                    }
+
+                    var destImage = new Bitmap(newWidth, newHeight);
+                    destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+                    using (Graphics g = Graphics.FromImage(destImage))
+                    {
+
+                        g.SmoothingMode = SmoothingMode.HighQuality;
+                        g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                        g.CompositingQuality = CompositingQuality.HighQuality;
+                        g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                        using (var wrapMode = new ImageAttributes())
+                        {
+                            wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                            g.DrawImage(image, new Rectangle(0, 0, newWidth, newHeight), 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                        }
+                        return imageToByteArray(destImage);
+                    }
+                }
+            }
+            catch
+            {
+                return null;
+            }
+
+        }
     }
 }
--- Alpha/Models/MenuViewModels.cs
+++ Alpha/Models/MenuViewModels.cs
@@ -26,5 +26,28 @@ namespace Alpha.Models
         [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage = "Valid Decimal number with maximum 2 decimal places.")]
         public string UnitPrice { get; set; }
 
+        [Display(Name = "Article image")]
+        public HttpPostedFileBase Image { get; set; }
+    }
+
+    public class EditItemViewModel
+    {
+
+        public int ItemId { get; set; }
+        [Required(ErrorMessage = "Enter a valide restaurant name")]
+        [Display(Name = "Article Name")]
+        public string Name { get; set; }
+        [Display(Name = "Should this article be available?")]
+        public bool IsAvailable { get; set; }
+
+        [Display(Name = "Article price")]
+        [Required(ErrorMessage = "Expenses is required.")]
+        [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage = "Valid Decimal number with maximum 2 decimal places.")]
+        public string UnitPrice { get; set; }
+
+        [Display(Name = "Article image")]
+        public HttpPostedFileBase Image { get; set; }
+
+
     }
 }
